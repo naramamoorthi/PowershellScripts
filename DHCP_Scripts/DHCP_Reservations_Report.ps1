a356c4af7b600e2a8bcaae37c262a26321855e4d
Navin Ramamoorthi <43849268+naramamoorthi@users.noreply.github.com>
2020-02-23 16:59:07 +0800

    Update DHCP_Reservations_Report.ps1

--- DHCP_Scripts/DHCP_Reservations_Report.ps1
+++ DHCP_Scripts/DHCP_Reservations_Report.ps1
@@ -1,1 +1,3 @@
 Get-DHCPServerv4Reservation -ScopeId 10.0.0.0 | Select ScopeID,Name,ClientID,Type,Description | Export-Csv C:\Temp\DHCP_Reservations.csv
+##The code below queries a specific DHCP server
+## Get-DHCPServerv4Serversatio -ScopeId 10.0.0.0 -ComputerName dhcp1.domain.com | fl >> C:\Temp\DHCP.txt
